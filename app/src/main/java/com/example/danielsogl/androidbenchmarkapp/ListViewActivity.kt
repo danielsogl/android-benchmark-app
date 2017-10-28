6ebb1e82da79f7cd436fdc1d1226d94df6de0306
Daniel <mytechde@outlook.com>
2017-10-28 08:56:11 +0200

    last stand

--- app/src/main/java/com/example/danielsogl/androidbenchmarkapp/ListViewActivity.kt
+++ app/src/main/java/com/example/danielsogl/androidbenchmarkapp/ListViewActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_list_view.*
+import java.util.*
 
 class ListViewActivity : AppCompatActivity() {
 
